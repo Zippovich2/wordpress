fd3fda5051a7458a73b64a1c373e6cadd96a5a25
Roman Skoropadskyi <zipo.ckorop@gmail.com>
2019-12-26 14:23:07 +0200

    init

---
+++ public/app/mu-plugins/register-theme-directory.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Register Theme Directory
+Version:     1.0.0
+Description: Register default themes directory
+*/
+
+register_theme_directory(ABSPATH . 'wp-content/themes');
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+/** Load wordpress core */
+define( 'WP_USE_THEMES', true );
+require( dirname( __FILE__ ) . '/wp/wp-blog-header.php' );
