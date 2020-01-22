cffc8998947613b878330573562b2240c3bb5aa7
Roman Skoropadskyi <zipo.ckorop@gmail.com>
2020-01-22 21:13:09 +0200

    updated wp-config.php add conflict in composer.json

--- public/wp-config.php
+++ public/wp-config.php
@@ -1,7 +1,7 @@
 <?php
 
 use WordpressWrapper\Loader\Loader;
-use Zippovich2\Wordpress\Config;
+use WordpressWrapper\Config\Config;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -12,5 +12,5 @@ $table_prefix = isset($_ENV['DB_PREFIX']) ? $_ENV['DB_PREFIX'] : 'wp_';
 
 require_once __DIR__ . '/wp/wp-settings.php';
 
-$config = new Config(PROJECT_ROOT . '/config');
+$config = new Config($_ENV['PROJECT_ROOT'] . '/config');
 $config->load();
