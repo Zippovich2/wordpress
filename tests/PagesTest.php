fac7a07c5ab13ef6fb9c666c55001adc29181a2d
Roman <zipo.ckorop@gmail.com>
2020-10-26 11:46:15 +0200

    Trying to fix build error

--- tests/PagesTest.php
+++ tests/PagesTest.php
@@ -26,16 +26,21 @@ class PagesTest extends TestCase
     public function setUp(): void
     {
         $this->httpClient = new Client([
-            'base_uri' => 'http://localhost:8080',
+            'base_uri' => $_ENV['APP_HTTP_HOST'],
             'timeout' => 10,
             'allow_redirects' => false,
         ]);
     }
 
+    public function tearDown(): void
+    {
+        $this->httpClient = null;
+    }
+
     private function createGetRequest(string $path): int
     {
         $ch = \curl_init();
-        \curl_setopt($ch, CURLOPT_URL, sprintf('http://localhost:8080%s', $path));
+        \curl_setopt($ch, CURLOPT_URL, $_ENV['APP_HTTP_HOST'] . $path);
         \curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         \curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
         \curl_exec($ch);
@@ -45,30 +50,27 @@ class PagesTest extends TestCase
         return $responseCode;
     }
 
-    public function tearDown(): void
-    {
-        $this->httpClient = null;
-    }
-
     /**
      * @dataProvider pageStatusCodeProvider
      */
-    public function testStatusCode($page, $expectedStatusCode): void
+    public function testStatusCode($page, $expectedStatusCode, $expectedLocation): void
     {
         $response = $this->httpClient->request('GET', $page);
         $code = $this->createGetRequest($page);
+        $location = $response->getHeaderLine('Location');
 
         static::assertEquals($expectedStatusCode, $response->getStatusCode());
         static::assertEquals($expectedStatusCode, $code);
+        static::assertEquals($expectedLocation, $location);
     }
 
     public function pageStatusCodeProvider()
     {
         return [
-            ['/', 200],
-            ['/wp/wp-login.php', 200],
-            ['/wp/', 301],
-            ['/wp/wp-admin/', 302],
+            ['/', 200, ''],
+            ['/wp/wp-login.php', 200, ''],
+            ['/wp/', 301,  $_ENV['APP_HTTP_HOST'] . '/wp/wp-admin/'],
+            ['/wp/wp-admin/', 302, $_ENV['APP_HTTP_HOST'] . '/wp/wp-login.php?redirect_to=http%3A%2F%2Flocalhost%3A8080%2Fwp%2Fwp-admin%2F&reauth=1'],
         ];
     }
 }
